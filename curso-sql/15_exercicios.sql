0fe2a171b1110078d31b9b8920adf38538422c03
FAyllan111 <franciscoayllan@gmail.com>
2026-07-17 10:42:36 -0300

    ..

---
+++ curso-sql/15_exercicios.sql
@@ -0,0 +1,8 @@
+Quantos clientes tem email cadastrado?
+Qual cliente juntou mais pontos positivos em 2025-05?
+Qual cliente fez mais transações no ano de 2024?
+Quantos produtos são de rpg?
+Qual o valor médio de pontos positivos por dia?
+Qual dia da semana quem mais pedidos em 2025?
+Qual o produto mais transacionado?
+Qual o produto com mais pontos transacionados?
